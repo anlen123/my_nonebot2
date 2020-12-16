5b1427c65a43e61cebabb8b9c40d2d99d037b33f
anlen123 <1761512493@qq.com>
2020-12-16 09:58:40 +0000

    update

--- gocp/go.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh 
-/root/nonebot2/gocp/go-cqhttp-v0.9.33-linux-amd64
-
--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-screen -dmS qqbot
+screen -X -S qqbot quit
+screen -X -S gocp quit
+screen -wipe
+screen -dmS qqbot /bin/zsh
 screen -x -S qqbot -p 0 -X stuff "cd /root/nonebot2/nb2/\n"
 screen -x -S qqbot -p 0 -X stuff "/root/miniconda3/envs/qqbot/bin/python bot.py\n"
-screen -dmS gocp
+screen -dmS gocp /bin/zsh
 screen -x -S gocp -p 0 -X stuff "cd /root/nonebot2/gocp/\n"
-screen -x -S gocp -p 0 -X stuff "./go-cqhttp-v0.9.33-linux-amd64\n" 
+screen -x -S gocp -p 0 -X stuff "./go\n" 
