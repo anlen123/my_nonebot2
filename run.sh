c65fa4b8aa6ab2f12c3f24e6fb0abaf7f17d3c19
anlen123 <1761512493@qq.com>
2021-01-15 09:34:33 +0000

    update

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ screen -wipe
 screen -dmS qqbot /bin/bash
 screen -x -S qqbot -p 0 -X stuff "cd /root/nonebot2/nb2/\n"
 screen -x -S qqbot -p 0 -X stuff "python bot.py\n"
+sleep 5
 screen -dmS gocp /bin/bash
 screen -x -S gocp -p 0 -X stuff "cd /root/nonebot2/gocp/\n"
 screen -x -S gocp -p 0 -X stuff "./go\n" 
