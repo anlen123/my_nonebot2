43fb61b947a3fa1aaf93de5daabf41bbd4bd0cf0
anlen123 <1761512493@qq.com>
2021-03-25 21:08:02 +0800

    :new: 修改了run.sh 之前的screen 改为tmux

--- nb2/plugins/love/love.py
+++ nb2/plugins/love/love.py
@@ -1,12 +1,9 @@
 from nonebot import on_command,on_startswith
 from nonebot.rule import to_me
 from nonebot.adapters.cqhttp import Bot, Event, MessageSegment, Message
-love = on_startswith(msg="love", priority=4, rule=to_me())
-from datetime import datetime
-import nonebot
 
 # 识别参数 并且给state 赋值
-
+love = on_startswith(msg="love", priority=4, rule=to_me())
 @love.handle()
 async def love_rev(bot: Bot, event: Event, state: dict):
     await love.finish(message="我也爱你"+Message("[CQ:face,id=214][CQ:face,id=66]"), at_sender=True)
--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
-screen -S nb2 -X quit
-screen -S gocp -X quit
-screen -wipe
-screen -dmS nb2 /bin/bash
-screen -x -S nb2 -p 0 -X stuff "cd /root/my_nonebot2/nb2/\n"
-screen -x -S nb2 -p 0 -X stuff "export ALL_PROXY=http://127.0.0.1:1081\n"
-screen -x -S nb2 -p 0 -X stuff "python bot.py\n"
-sleep 5
-screen -dmS gocp /bin/bash
-screen -x -S gocp -p 0 -X stuff "cd /root/my_nonebot2/gocp/\n"
-screen -x -S gocp -p 0 -X stuff "./go\n" 
+#screen -S nb2 -X quit
+#screen -S gocp -X quit
+#screen -wipe
+#screen -dmS nb2 /bin/bash
+#screen -x -S nb2 -p 0 -X stuff "cd /root/my_nonebot2/nb2/\n"
+#screen -x -S nb2 -p 0 -X stuff "export ALL_PROXY=http://127.0.0.1:1081\n"
+#screen -x -S nb2 -p 0 -X stuff "python bot.py\n"
+#sleep 5
+#screen -dmS gocp /bin/bash
+#screen -x -S gocp -p 0 -X stuff "cd /root/my_nonebot2/gocp/\n"
+#screen -x -S gocp -p 0 -X stuff "./go\n" 
+
+
+
+tmux kill-pane -t nb2 
+tmux kill-pane -t gocp
+
+tmux new -s nb2 -d 
+tmux new -s gocp  -d 
+
+tmux send -t "nb2" "export ALL_PROXY=http://127.0.0.1:1081;cd /root/my_nonebot2/nb2/; python bot.py" Enter
+tmux send -t "gocp" "cd /root/my_nonebot2/gocp/ ; ./go" Enter
+
+
