5beb7d04e1bd2b3df1273ec77916582863e66ba0
anlen123 <1761512493@qq.com>
2026-04-03 11:57:50 +0800

    'fix'

--- bot.py
+++ bot.py
@@ -9,7 +9,9 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(OneBot_V11_Adapter)
 
-nonebot.load_from_toml("pyproject.toml")
+
+
+# nonebot.load_from_toml("pyproject.toml")
  
 # nonebot.load_plugin("nonebot_plugin_pixiv")
 nonebot.load_plugin('nonebot_plugin_ygo')
@@ -17,22 +19,24 @@ nonebot.load_plugin('nonebot_plugin_apscheduler')
 nonebot.load_plugin('nonebot_plugin_abbrreply') 
 nonebot.load_plugin('nonebot_plugin_plus_one')
 nonebot.load_plugin('nonebot_plugin_navicat') 
-nonebot.load_plugin('nonebot_plugin_fishing')
+# nonebot.load_plugin('nonebot_plugin_fishing')
  
 nonebot.load_plugin("plugins.love") 
-nonebot.load_plugin("plugins.jm")
+# nonebot.load_plugin("plugins.jm")
 nonebot.load_plugin("plugins.nonebot_plugin_pixiv.nonebot_plugin_pixiv")
 
 nonebot.load_plugin("plugins.nonebot_plugin_sbbot")
-nonebot.load_plugin("plugins.deepseek_gpt")
-nonebot.load_plugin("plugins.gemini_gpt")
+# nonebot.load_plugin("plugins.deepseek_gpt")
+# nonebot.load_plugin("plugins.gemini_gpt")
 nonebot.load_plugin("plugins.nonebot_plugin_biliav")
-nonebot.load_plugin("plugins.nonebot_plugin_picsearcher.nonebot_plugin_picsearcher")
+# nonebot.load_plugin("plugins.nonebot_plugin_picsearcher.nonebot_plugin_picsearcher")
 nonebot.load_plugin("plugins.nonebot_plugin_masterduel.nonebot_plugin_masterduel")
+# nonebot.load_plugin(
+#     "plugins.nonebot-plugin-terralink.nonebot_plugin_terralink")
 # nonebot.load_plugin("plugins.nonebot_plugin_command") #看情况恢复吧， win上使用太危险了
 nonebot.load_plugin("plugins.nonebot_plugin_xuanran")
 nonebot.load_plugin("plugins.nonebot_plugin_yulu")
-nonebot.load_plugin('plugins.nonebot_plugin_pokemon')
+# nonebot.load_plugin("plugins.terraria_log_monitor")
   
 if __name__ == "__main__":
     # nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
--- plugins/nonebot_plugin_masterduel/nonebot_plugin_masterduel/config.py
+++ plugins/nonebot_plugin_masterduel/nonebot_plugin_masterduel/config.py
@@ -3,8 +3,8 @@ from nonebot import get_plugin_config
 
 
 class Config(BaseModel):
-    nonebot_plugin_masterduel_root_dir: str = "D:\\nb2\\my_nonebot2\\plugins\\nonebot_plugin_masterduel\\nonebot_plugin_masterduel"
-    nonebot_plugin_masterduel_img_dir: str = "D:\\nb2\\my_nonebot2\\plugins\\nonebot_plugin_masterduel\\nonebot_plugin_masterduel\\img"
+    nonebot_plugin_masterduel_root_dir: str = "C:\\Users\\Administrator\\Desktop\\nb2\\my_nonebot2\\plugins\\nonebot_plugin_masterduel\\nonebot_plugin_masterduel"
+    nonebot_plugin_masterduel_img_dir: str = "C:\\Users\\Administrator\\Desktop\\nb2\\my_nonebot2\\plugins\\nonebot_plugin_masterduel\\nonebot_plugin_masterduel\\img"
     nonebot_plugin_masterduel_img_card_dir: str = "D:\\MyCardLibrary\\ygopro2\\picture\\card"
 
 
